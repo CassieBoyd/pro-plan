542b291f37656ee3337e4fd321afff6dbc1c724f
Cassie Boyd <cassandra.bacon@gmail.com>
2019-12-10 15:52:58 -0600

    Have things rendering to the DOM

--- src/Pro-Plan.js
+++ src/Pro-Plan.js
@@ -1,28 +1,20 @@
-// Renamed from App.js
+import React, { Component } from 'react'
+import './Pro-Plan.css'
 
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ProPlan extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Student Kennels<br />
+                    <small>Loving care when you're not there.</small>
+                </h2>
+                <address>
+                    Visit Us at the Nashville North Location
+                    <br />500 Puppy Way
+                </address>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default ProPlan
