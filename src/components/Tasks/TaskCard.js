a9593a20fca305136996a77d3a90533f0193793f
Cassie Boyd <cassandra.bacon@gmail.com>
2019-12-14 12:44:22 -0600

    Added TaskCard

---
+++ src/components/Tasks/TaskCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TaskCard extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h3>Task: <span className="card-taskname">{this.props.task.name}</span></h3>
+          <p>Note: {this.props.task.taskNote}</p>
+        </div>
+        <a href={`/tasks/${this.props.task.id}`} className="stretched-link"></a>
+      </div>
+    );
+  }
+}
+
+export default TaskCard;
