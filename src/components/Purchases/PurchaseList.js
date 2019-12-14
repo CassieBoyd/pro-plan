d87c7c58f96c1ef5e45637d425905a4330c8211f
Cassie Boyd <cassandra.bacon@gmail.com>
2019-12-14 16:37:13 -0600

    Added PurchaseList

---
+++ src/components/Purchases/PurchaseList.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+//import the components we will need
+import PurchaseCard from "./PurchaseCard";
+import PurchaseManager from "../modules/PurchaseManager";
+import ActionBar from "../Nav/ActionBar";
+import OptionBar from "../Nav/OptionBar";
+
+class PurchaseList extends Component {
+  state = {
+    purchases: []
+  };
+
+  addItem = () => {
+    this.props.history.push(`/projects/${this.props.projectId}/purchases/new`);
+  };
+  componentDidMount() {
+    console.log("Purchase List: ComponentDidMount");
+
+    PurchaseManager.getAll(this.props.projectId).then(purchases => {
+      this.setState({
+        purchases: purchases
+      });
+    });
+  }
+
+  render() {
+    console.log("Purchase List: Render");
+
+    return (
+      <div className="container-cards">
+          <OptionBar />
+        {this.state.purchases.map(purchase => (
+          <PurchaseCard key={purchase.id} purchase={purchase} {...this.props}/>
+        ))}
+        <ActionBar addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default PurchaseList;
